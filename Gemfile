70bd7a0f49dc6f3990e14ed5bd43dda3f1089f8f
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-17 10:14:58 +1100

    initial

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "http://rubygems.org"
+
+# Specify your gem's dependencies in neo4j-will_paginate.gemspec
+gemspec
---
+++ lib/neo4j-will_paginate.rb
@@ -0,0 +1,7 @@
+require "neo4j-will_paginate/version"
+
+module Neo4j
+  module WillPaginate
+    # Your code goes here...
+  end
+end
